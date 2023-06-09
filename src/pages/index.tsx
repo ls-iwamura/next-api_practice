d67de8239cef2ade2a31dd468ddbc38bb9be7edd
LSW-024\iwamura <iwamura@logical-studio.com>
2023-06-09 17:26:14 +0900

    課題用にまっさらにする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,9 @@
-import Head from "next/head";
-import styles from "./Home.module.css";
-import { PokemonList } from "@/components/PokemonList/PokemonList";
-import React from 'react'
-
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>ポケモン一覧</title>
-        <meta name="description" content="ポケモンの一覧ページです。" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={styles.container}>
-        <PokemonList />
-      </main>
-    </>  )
-}
+      <main></main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
